0b8ef9a652468662414177788a947f42961c5259
fominvic81 <fomin.vic81@gmail.com>
2025-11-05 22:02:12 +0200

    lab5

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ void main() {
   runApp(const MyApp());
 }
 
+const _size = 25.0;
+const _bgColor = Colors.green;
+const _borderWidth = 3.0;
+const _borderColor = Colors.grey;
+const _borderRadius = 4.0;
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -31,14 +37,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter += 2;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,23 +44,76 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.mouse),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              _buildDot(),
+              _buildDot(),
+              _buildDot(),
+              _buildEmpty(),
+              //
+            ],
+          ),
+          Row(
+            children: [
+              _buildEmpty(),
+              _buildEmpty(),
+              _buildEmpty(),
+              _buildDot(),
+            ],
+          ),
+          Row(
+            children: [
+              _buildEmpty(),
+              _buildEmpty(),
+              _buildDot(),
+              _buildEmpty(),
+            ],
+          ),
+          Row(
+            children: [
+              _buildEmpty(),
+              _buildDot(),
+              _buildEmpty(),
+              _buildEmpty(),
+            ],
+          ),
+          Row(
+            children: [
+              _buildDot(),
+              _buildEmpty(),
+              _buildEmpty(),
+              _buildEmpty(),
+            ],
+          ),
+          Row(
+            children: [
+              _buildDot(),
+              _buildDot(),
+              _buildDot(),
+              _buildDot(),
+              //
+            ],
+          ),
+        ],
       ),
     );
+  }
+
+  Widget _buildDot() {
+    return Container(
+      width: _size,
+      height: _size,
+      decoration: BoxDecoration(
+        color: _bgColor,
+        borderRadius: const BorderRadius.all(Radius.circular(_borderRadius)),
+        border: Border.all(color: _borderColor, width: _borderWidth),
+      ),
+    );
+  }
+
+  Widget _buildEmpty() {
+    return const SizedBox(width: _size, height: _size);
   }
 }
