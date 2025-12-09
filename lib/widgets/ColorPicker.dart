a313179677bdefe08ebe2f6e16c7f2ea4e70eed9
fominvic81 <fomin.vic81@gmail.com>
2025-12-09 21:04:38 +0200

    lab7

--- lib/widgets/ColorPicker.dart
+++ lib/widgets/ColorPicker.dart
@@ -8,12 +8,38 @@ class ColorPicker extends StatefulWidget {
 }
 
 class ColorPickerState extends State<ColorPicker> {
+  int _r = 0;
+  int _g = 0;
+  int _b = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: SizedBox(
+            width: 200,
+            height: 200,
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, _r, _g, _b),
+                )
+            ),
+          ),
+        ),
+        Text("Red $_r"),
+        Slider(value: _r.toDouble() / 255, onChanged: (value) => setState(() {
+          _r = (value * 255).floor();
+        })),
+        Text("Green $_g"),
+        Slider(value: _g.toDouble() / 255, onChanged: (value) => setState(() {
+          _g = (value * 255).floor();
+        })),
+        Text("Blue $_b"),
+        Slider(value: _b.toDouble() / 255, onChanged: (value) => setState(() {
+          _b = (value * 255).floor();
+        })),
       ],
     );
   }
