e29412364496df7cbcbb4a4802774eab0cb25aa0
fominvic81 <fomin.vic81@gmail.com>
2025-12-09 20:14:06 +0200

    tpl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './widgets/signin_screen.dart';
+import 'package:lntu_flutter/widgets/ColorPicker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
       ),
-      home: const SigninScreen(),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: ColorPicker(),
+      ),
     );
   }
 }
---
+++ lib/widgets/ColorPicker.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+class ColorPicker extends StatefulWidget {
+  @override
+  State<ColorPicker> createState() => ColorPickerState();
+}
+
+class ColorPickerState extends State<ColorPicker> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
